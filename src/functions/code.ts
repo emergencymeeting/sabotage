996c32aefd5575c0f3c8822296627ff0dec97d07
Joel Califa <jcalifa@gmail.com>
2020-10-28 17:05:05 -0400

    initial thing

--- src/functions/code.ts
+++ src/functions/code.ts
@@ -1,11 +1,51 @@
 import { Message } from 'discord.js'
 import { Bot } from '../lib/bot'
 
+const codeRegex = /^[A-Za-z]{6}$/
+
 /**
  * 🤔
  */
 export function code(bot: Bot) {
-  bot.command('code', async (_: string, msg: Message) => {
-    bot.log.info('Someone said .code?', msg)
+  // .code <....>
+  bot.command('code', async (args: string, msg: Message) => {
+    if (!msg.guild) return
+
+    const [number, code] = args.split(' ')
+
+    if (code && !codeRegex.test(code)) {
+      return msg.channel.send(`**\`${code}\`** is not a valid code (6 letters)`)
+    }
+
+    const voiceChannel = msg.guild.channels.cache.find((channel) => {
+      return (
+        channel.type === 'voice' && channel.name.startsWith(`Game ${number}`)
+      )
+    })
+
+    if (!voiceChannel) {
+      return msg.channel.send(
+        `Voice channel **Game ${number}** does not exist!`
+      )
+    }
+
+    if (code) {
+      // Game 1 (ABCDEQ)
+      const uppercaseCode = code.toUpperCase()
+      await voiceChannel.setName(`Game ${number} (${uppercaseCode})`)
+      return msg.channel.send(
+        `Game ${number}'s invite code is now set to **\`${uppercaseCode}\`**!`
+      )
+    } else {
+      // Game 1
+      await voiceChannel.setName(`Game ${number}`)
+      return msg.channel.send(`Game ${number}'s invite code has been reset`)
+    }
   })
+
+  // When someone leaves the voice call, if they're the last one to leave,
+  // bot.on('disconnect', () => {
+  // Someone left the voice call
+
+  // })
 }
