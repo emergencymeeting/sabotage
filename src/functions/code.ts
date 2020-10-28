6d011dd5ef23c5def7d813f3e7a0e66fa1bbeee0
Jason Etcovitch <jasonetco@github.com>
2020-10-28 17:50:01 -0400

    Tweak some more comments

--- src/functions/code.ts
+++ src/functions/code.ts
@@ -1,28 +1,32 @@
-import { Message } from 'discord.js'
 import { Bot } from '../lib/bot'
 
 const codeRegex = /^[A-Za-z]{6}$/
 
 /**
- * 🤔
+ * Manages Among Us voice channel names by appending/removing
+ * a lobby code from the channel name.
  */
 export function code(bot: Bot) {
-  // .code <....>
-  bot.command('code', async (args: string, msg: Message) => {
+  // .code <number> <code>
+  bot.command('code', async (args, msg) => {
     if (!msg.guild) return
 
+    // Get the number and code
     const [number, code] = args.split(' ')
 
+    // If there is a code and its invalid, tell them
     if (code && !codeRegex.test(code)) {
       return msg.channel.send(`**\`${code}\`** is not a valid code (6 letters)`)
     }
 
+    // Find the voice channel named `Game <number>`
     const voiceChannel = msg.guild.channels.cache.find((channel) => {
       return (
         channel.type === 'voice' && channel.name.startsWith(`Game ${number}`)
       )
     })
 
+    // No voice channel, no happiness
     if (!voiceChannel) {
       return msg.channel.send(
         `Voice channel **Game ${number}** does not exist!`
@@ -30,21 +34,23 @@ export function code(bot: Bot) {
     }
 
     if (code) {
-      // Game 1 (ABCDEQ)
+      // Set the channel name to Game 1 (ABCDEQ)
       const uppercaseCode = code.toUpperCase()
       await voiceChannel.setName(`Game ${number} (${uppercaseCode})`)
       return msg.channel.send(
         `Game ${number}'s invite code is now set to **\`${uppercaseCode}\`**!`
       )
     } else {
-      // Game 1
+      // Set the channel name to Game 1
       await voiceChannel.setName(`Game ${number}`)
       return msg.channel.send(`Game ${number}'s invite code has been reset`)
     }
   })
 
-  // When someone leaves the voice call, if they're the
-  // last one to leave, reset the name
+  /**
+   * When someone leaves the voice call, if they're the
+   * last one to leave, reset the name
+   */
   bot.on('voiceStateUpdate', (voiceState) => {
     if (!voiceState.channel) return
     // Someone left the voice call, check that its empty now
